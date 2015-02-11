0df454b7a492b26b441cdaff14bb5e58c19b3627
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2015-02-11 23:39:30 +0100

    Livereload, Connect and Watch solved

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,8 @@
-var livereloadPort = 35729;
-var lrSnippet = require('connect-livereload')({
-  port: livereloadPort
-});
-var mountFolder = function(connect, dir){
-  return connect.static(require('path').resolve(dir));
-}
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('connect-livereload');  
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
@@ -21,15 +13,14 @@ module.exports = function(grunt) {
       options: {
         port: 8000,
         hostname: '*',
+        base: 'build',
         open: {
-          target: 'http://localhost:8000'            
+          target: 'http://localhost:8000/'
         }
       },
       livereload: {
         options: {
-          middleware: function(connect){
-            return [lrSnippet, mountFolder(connect, 'build')];
-          }
+          open: true
         }
       }
     },
@@ -43,29 +34,24 @@ module.exports = function(grunt) {
     },
     
     watch: {
-      options: {
-        livereload: true,
-      },
       html: {
         files: ['src/*.html'],
         tasks: ['copy:html'],
         options: {
+          livereload: true,
           spawn: false
         }
       },
       sass:{
-        files:['src/sass/**/*'],
-        tasks:['compass:dist']
-      },
-      livereload: {
+        files: ['src/sass/**/*'],
+        tasks: ['compass:dev'],
         options:{
-          livereload: livereloadPort
-        },
-        files:['build/*.html']
-      }
+          livereload: true
+        }
+      },
     },
 
-    compass: {                  
+    compass: {
       dev:{
         options:{
           sassDir: 'src/sass',
