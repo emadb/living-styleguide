674991cb7306233c58d14c15f2580a0344c22d43
Maddalena <maddalena@codiceplastico.com>
2015-02-11 15:12:47 +0100

    configurato grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,20 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+     connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          keepalive: true
+        }
+      }
+    }
   });
 
   // Default task(s).
-  grunt.registerTask('default', function(){
-    console.log('Hello World');
-  });
+  grunt.registerTask('default', ['connect']);
 
+  grunt.loadNpmTasks('grunt-contrib-connect');
 };
