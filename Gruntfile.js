7046be69b4f9f0a5003081bd91e9757b6a8ffb07
Maddalena <maddalena@codiceplastico.com>
2015-02-11 16:02:04 +0100

    aggiunto watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,62 @@
+var livereloadPort = 35729;
+var lrSnippet = require('connect-livereload')({
+  port: livereloadPort
+});
+var mountFolder = function(connect, dir){
+  return connect.static(require('path').resolve(dir));
+}
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('connect-livereload');
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-     connect: {
-      server: {
+    connect: {
+      options: {
+        port: 8000,
+        hostname: '*',
+        open: {
+          target: 'http://localhost:8000'            
+        }
+      },
+      livereload: {
         options: {
-          port: 8000,
-          hostname: '*',
-          keepalive: true,
-          base: 'build', 
-          open: {
-            target: 'http://localhost:8000'            
+          middleware: function(connect){
+            return [lrSnippet, mountFolder(connect, 'build')];
           }
         }
       }
     },
+
     copy: {
-      main: {
+      html: {
         files: [
           {expand: true, src: ['src/*.html'], dest: 'build/', flatten: true}
         ]
+      }
+    },
+    
+    watch: {
+      html: {
+        files: ['src/*.html'],
+        tasks: ['copy:html'],
+        options: {
+          spawn: false
+        }
+      },
+      livereload: {
+        options:{
+          livereload: livereloadPort
+        },
+        files:['build/*.html']
       }
     }
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['copy:main', 'connect']);
+  grunt.registerTask('default', ['copy:html', 'connect:livereload', 'watch']);
 };
